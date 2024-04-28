d5ba461f55ebf549781500a534c06c568e3ad610
TaYaKi71751 <shantian.tw@gmail.com>
2024-04-28 18:24:06 +0900

    Add index.css

--- util.js
+++ util.js
@@ -1,29 +1,32 @@
 const Sunday = {
-    getDate: function (week, month, year) {
-        let timestamp = Date.parse(new Date(`KST ${year}-${month}-${1}`).toString());
-        let date = new Date();
-        let day = date.getDay();
-        for (let i = 0; i < week; i++) {
-            do {
-                date = new Date(timestamp + 86400000);
-                timestamp = Date.parse(date);
-                if (date.getDay() == NaN) throw Error('');
-            } while (date.getDay() != 0);
-            if (date.getMonth() + 1 != month) throw new Error('Cannot reach');
-        }
-        return date;
-    }
+	getDate: function (week, month, year) {
+		let timestamp = Date.parse(new Date(`KST ${year}-${month}-${1}`).toString());
+		let date = new Date();
+		let day = date.getDay();
+		for (let i = 0; i < week; i++) {
+			do {
+				date = new Date(timestamp + 86400000);
+				timestamp = Date.parse(date);
+				if (date.getDay() == NaN) throw Error('');
+			} while (date.getDay() != 0);
+			if (date.getMonth() + 1 != month) throw new Error('Cannot reach');
+		}
+		return date;
+	}
 };
 let now = new Date();
+const container = document.querySelector('.container')
 if (
-    now.getDate() == Sunday.getDate(2, now.getMonth() + 1, now.getFullYear()).getDate() ||
-    now.getDate() == Sunday.getDate(4, now.getMonth() + 1, now.getFullYear()).getDate()
+	now.getDate() == Sunday.getDate(2, now.getMonth() + 1, now.getFullYear()).getDate() ||
+	now.getDate() == Sunday.getDate(4, now.getMonth() + 1, now.getFullYear()).getDate()
 ) {
-    // holiday
-    console.log('HOLIDAY');
-    document.body.style.background = '#FF9FAC';
+	// holiday
+	console.log('HOLIDAY');
+	document.body.style.background = '#FF9FAC';
+	container.innerHTML = 'HOLIDAY';
 } else {
-    // not holiday
-    console.log('NOT HOLIDAY');
-    document.body.style.background = '#9FFFAC';
+	// not holiday
+	console.log('NOT HOLIDAY');
+	document.body.style.background = '#9FFFAC';
+	container.innerHTML = 'NOT HOLIDAY';
 }
